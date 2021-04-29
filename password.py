e9e165e53b06417601d4b8b00442a4ff0625023d
Priya Guo <priya.guo@gmail.com>
2021-04-29 15:13:23 +0800

    third

--- password.py
+++ password.py
@@ -1,14 +1,14 @@
-# while True 版本
+# while 版本
 password = 'a123456'
 i = 3
-
-while True:
+while i > 0:
+    i = i - 1
     pwd = input('請輸入密碼:')
     if pwd == password:
         print('登入成功!')
         break
     else:
-        i = i - 1
-        print('密碼錯!還有', i,'次機會')
-        if i == 0 :
-            break
+        if i > 0:
+            print('密碼錯誤!還有', i,'次機會')
+        else:
+            print('密碼錯誤!你沒機會了')
